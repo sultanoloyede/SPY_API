1a5b3875f7fc0c0df426fc6021d199abf71399a7
JDazogbo <jdazo054@uottawa.ca>
2025-07-17 18:49:49 -0400

    Updated to see debug prints

--- IB/trading.py
+++ IB/trading.py
@@ -5,57 +5,17 @@ from ibapi.order import *
 
 import threading
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 import pytz
 import math
-import ta_py as ta
+import ta
+import dateutil
 
 import numpy as np
 import pandas as pd
 
-# IB Api Overwrite
-class IBApi(EWrapper, EClient):
-
-    orderId = 1
-
-    def __init__(self, bot_callback):
-        EClient.__init__(self, self)
-        self.bot_callback = bot_callback
-    
-    # Gets Data for previous time window
-    def historicalData(self, requestId, bar):
-        try:
-            bot.on_bar_update(requestId, bar, False)
-        except Exception as e:
-            print(e)
-    
-    # Gets Live data updates
-    def historicalDataUpdate(self, requestId, bar):
-        try:
-            bot.on_bar_update(requestId, bar, False)
-        except Exception as e:
-            print(e)
-    
-    # Gets Historical data end
-    def historicalDataEnd(self, requestId, start, end):
-        print(requestId)
-    
-    def nextValidId(self, nextorderId):
-        self.orderId = nextorderId
-        
-    def realtimeBar(self, reqId, barTime, open_, high, low, close, volume, wap, count):
-        super().realtimeBar(reqId, barTime, open_, high, low, close, volume, wap, count)
-        try:
-            bot.on_bar_update(reqId, barTime, open_, high, low, close, volume, wap, count)
-        except Exception as e:
-            print(e)
-    def error(self, id, errorCode, errorMsg, advancedOrderRejectJson=""):
-        print(errorCode)
-        print(errorMsg)
-
 # Bar Object
 class Bar:
-
     def __init__(self):
         self.open = 0
         self.low = 0
@@ -65,148 +25,161 @@ class Bar:
         self.date = ''
 
 
+# IB API Overwrite
+class IBApi(EWrapper, EClient):
+    orderId = 1
+
+    def __init__(self, bot_callback):
+        EClient.__init__(self, self)
+        self.bot_callback = bot_callback
+
+    def historicalData(self, requestId, bar):
+        try:
+            bot.on_bar_update(requestId, bar, False)
+        except Exception as e:
+            print(e)
+
+    def historicalDataUpdate(self, requestId, bar):
+        try:
+            bot.on_bar_update(requestId, bar, True)
+        except Exception as e:
+            print(e)
+
+    def historicalDataEnd(self, requestId, start, end):
+        print(f"Request ID: {requestId}")
+
+    def nextValidId(self, nextorderId):
+        self.orderId = nextorderId
+
+    def error(self, id, errorCode, errorMsg, advancedOrderRejectJson=""):
+        print(errorCode)
+        print(errorMsg)
+
 
 # Bot Logic
 class Bot:
-
-    barSize = 1
-    barsArray = []
-    currentBar = Bar()
-    smaPeriod = 50
-    initialBarTime = datetime.now().astimezone(pytz.timezone("America/New_York"))
-    contract = Contract()
-
     def __init__(self):
-        # Create the IB API client and pass in the callback
-        self.ib = IBApi(self.on_bar_update)
+        self.barSize = 1
+        self.barsArray = []
+        self.currentBar = Bar()
+        self.smaPeriod = 10
+        self.initialBarTime = datetime.now().astimezone(pytz.timezone("US/Eastern"))
+        self.contract = Contract()
 
-        # Connect to TWS or IB Gateway
+        self.ib = IBApi(self.on_bar_update)
         self.ib.connect("127.0.0.1", 7497, 1)
 
-        # Start the network loop in a background thread
         ib_thread = threading.Thread(target=self.run_loop, daemon=True)
         ib_thread.start()
 
-        # Delay for connection
         time.sleep(1)
 
-        # Get symbol and bar size from user
-        self.barSize = input("Enter the bar size: ").upper()
-        symbol = input("Enter the base currency (e.g., EUR for EUR/USD): ").upper()
-        quote = input("Enter the quote currency (e.g., USD for EUR/USD): ").upper()
+        self.barSize = 1 # int(input("Enter the bar size: "))
+        symbol = "USD" # input("Enter the base currency (e.g., EUR for EUR/USD): ").upper()
+        quote = "CAD" # input("Enter the quote currency (e.g., USD for EUR/USD): ").upper()
 
-        # Setting up min text
-        mintext = " min"
-        if (int(self.barSize) > 1):
-            mintext = " mins"
+        mintext = " mins" if self.barSize > 1 else " min"
 
-        # Create contract
         self.contract.secType = "CASH"
         self.contract.symbol = symbol
         self.contract.exchange = "IDEALPRO"
         self.contract.currency = quote
-        self.ib.reqIds(-1)
-        
-        self.ib.reqHistoricalData(self.ib.orderId, self.contract, "", "2 D", str(self.barSize)+mintext, "MIDPOINT", 1, True, True, [])
 
+        self.ib.reqIds(-1)
+        self.ib.reqHistoricalData(self.ib.orderId, self.contract, "", "2 D", f"{self.barSize}{mintext}", "MIDPOINT", 1, True, True, [])
 
     def run_loop(self):
         self.ib.run()
-    
+
     def bracketOrder(self, parentOrderId, action, quantity, profitTarget, stopLoss):
-        # Create Parent Order
         parentOrder = Order()
         parentOrder.orderId = parentOrderId
-        parentOrder.orderType = "MKT" # Buy at Market (MKT) price
+        parentOrder.orderType = "MKT"
         parentOrder.action = action
         parentOrder.totalQuantity = quantity
         parentOrder.transmit = False
-        
-        # Profit Target Order
+
         profitOrder = Order()
         profitOrder.orderId = parentOrderId + 1
-        profitOrder.orderType = "LMT" # Buy at Limit (LMT) price
+        profitOrder.orderType = "LMT"
         profitOrder.lmtPrice = round(profitTarget, 2)
         profitOrder.action = "SELL"
         profitOrder.totalQuantity = quantity
         profitOrder.transmit = False
-    
-        # Stop Loss Order
+
         stopLossOrder = Order()
         stopLossOrder.orderId = parentOrderId + 2
-        stopLossOrder.orderType = "STP" # Buy at Stop (STP) price
+        stopLossOrder.orderType = "STP"
         stopLossOrder.lmtPrice = round(stopLoss, 2)
         stopLossOrder.action = "SELL"
         stopLossOrder.totalQuantity = quantity
         stopLossOrder.transmit = True
 
-        bracketOrders = [parentOrder, profitOrder, stopLossOrder]
-        return bracketOrders
-
-
+        return [parentOrder, profitOrder, stopLossOrder]
 
     def on_bar_update(self, reqId, bar, realtime):
-        if (realtime == False):
+        if not realtime:
             self.barsArray.append(bar)
-        else:
-            bartime = datetime.strptime(bar.date, "%Y%m%d %H:%M:%S").astimezone(pytz.timezone("America/New_York"))
-            minutes_diff = (bartime-self.initialBarTime).total_seconds() / 60.0
-            self.currentBar.date = bartime
+            return
+        
+        parts = bar.date.split(' ')
+        bartime = datetime.strptime(parts[0]+' ' + parts[1], "%Y%m%d %H:%M:%S").replace(tzinfo=dateutil.tz.gettz(parts[2]))
+        minutes_diff = (bartime-self.initialBarTime).total_seconds() / 60.0
+        self.currentBar.date = bartime
 
-            # On Bar close
-            if (minutes_diff > 0 and math.floor(minutes_diff) % self.barSize == 0):
-                # Entry - If we have a higher high, a higher low and we cross the 50 SMA, we buy
 
-                # Computing Simple Moving Average (SMA)
-                closes = []
-                for bar in self.barsArray:
-                    closes.append(bar.close)
-                self.close_array = pd.Series(np.asarray(closes))
-                self.sma = ta.trend.sma(self.close_array, self.smaPeriod, True)
-                print("SMA: " + str(self.sma[len(self.sma)-1]))
+        if minutes_diff > 0 and math.floor(minutes_diff) % self.barSize == 0:
+            self.currentBar.close = bar.close
+            self.currentBar.volume = bar.volume
+            self.barsArray.append(self.currentBar)
+            self.currentBar = Bar()
 
-                # Calculate the higher highs and higher lows
-                lastLow = self.barsArray[len(self.barsArray)-1].low
-                lastHigh = self.barsArray[len(self.barsArray)-1].high
-                lastClose = self.barsArray[len(self.barsArray)-1].close
-                lastBar = self.barsArray[len(self.barsArray)-1]
+            closes = [b.close for b in self.barsArray]
+            close_array = pd.Series(np.asarray(closes))
+            sma = ta.trend.SMAIndicator(close_array, self.smaPeriod, fillna=True).sma_indicator()
 
-                # Check criteria
-                if (bar.close > lastHigh 
-                    and self.currentBar.low > lastLow 
-                    and bar.close > str(self.sma[len(self.sma)-1])
-                    and lastClose < str(self.sma[len(self.sma)-2])):
+            if len(self.barsArray) < 2:
+                return
 
-                    # Bracket order of 2% Profit Target and 1% Stop Loss
-                    profitTarget = bar.close * 1.02
-                    stopLoss = bar.close*0.99
-                    quantity = 25000 # Minimum value for ForEx
+            lastBar = self.barsArray[-2]
+            lastLow = lastBar.low
+            lastHigh = lastBar.high
+            lastClose = lastBar.close
 
-                    bracket = self.bracketOrder(self.ib.orderId, "BUY", quantity, profitTarget, stopLoss)
+            print(f"bar.close: {bar.close}, lastHigh: {lastHigh}")
+            print(f"self.currentBar.low: {self.currentBar.low}, lastLow: {lastLow}")
+            print(f"bar.close: {bar.close}, SMA[-1]: {sma.iloc[-1]}")
+            print(f"lastClose: {lastClose}, SMA[-2]: {sma.iloc[-2]}")
+            print("SMA: " + str(sma.iloc[-1]))
 
-                    # Place bracket order
-                    for order in bracket:
-                        order.ocaGroup = "OCA_" + str(self.ib.orderId)
-                        order.ocaType = 2
-                        self.ib.placeOrder(order.orderId, self.contract, order)
-                    
-                    self.ib.orderId += 3
+            if (bar.close > lastHigh and
+                self.currentBar.low > lastLow and
+                bar.close > sma.iloc[-1] and
+                lastClose < sma.iloc[-2]):
 
-                    # Update the current bars
-                    self.currentBar.close = bar.close
-                    if self.currentBar.date != lastBar.date:
-                        print("New Bar")
-                        self.bars.append(self.currentBar)
-                    self.currentBar.open = bar.open
-        # Build Realtime bar
+                profitTarget = bar.close * 1.02
+                stopLoss = bar.close * 0.99
+                quantity = 25000
+                print("Placing Order")
+                bracket = self.bracketOrder(self.ib.orderId, "BUY", quantity, profitTarget, stopLoss)
+                print("Orders Placed")
+
+                for order in bracket:
+                    order.ocaGroup = "OCA_" + str(self.ib.orderId)
+                    order.ocaType = 2
+                    self.ib.placeOrder(order.orderId, self.contract, order)
+
+                self.ib.orderId += 3
+
+        # Build current bar
         if self.currentBar.open == 0:
             self.currentBar.open = bar.open
-        if self.currentBar.high == 0 or bar.high > self.currentBar.high:
             self.currentBar.high = bar.high
-        if self.currentBar.low == 0 or bar.low < self.currentBar.low:
             self.currentBar.low = bar.low
+        else:
+            self.currentBar.high = max(self.currentBar.high, bar.high)
+            self.currentBar.low = min(self.currentBar.low, bar.low)
 
 
-# Start bot
 if __name__ == "__main__":
     bot = Bot()
