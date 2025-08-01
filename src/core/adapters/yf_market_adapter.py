197fe52bdcb7e3674c4d27750367ee859fd42b23
Josue Dazogbo <jdazo054@uottawa.ca>
2025-08-01 12:03:43 -0400

    Added yf market adapter

---
+++ src/core/adapters/yf_market_adapter.py
@@ -0,0 +1,36 @@
+from src.core.ports.market_data_port import MarketDataPort
+from src.core.models.asset import Asset, AssetType
+from src.core.models.bar import Bar
+import yfinance as yf
+import tempfile
+from datetime import datetime, timedelta
+
+class YFMarketDataAdapter(MarketDataPort):
+
+    def __init__(self, asset: Asset):
+        self.asset = Asset
+        self._list_data:list[Bar] = []
+        self._counter: int = 0
+
+    def next_bar(self, symbol: str) -> Bar:
+        bar = self._list_data[self._counter]
+        self._counter += 1
+        return bar
+
+    def request_historical_data(self, asset: Asset, start_date: datetime, end_date: datetime):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            if asset.asset_type == AssetType.FOREX: # We are dealing with forex
+                stock = asset.symbol+asset.currency+"=X" # Write down the forex format for yfs
+                data = yf.download(stock, start_date, end_date)
+                self._list_data = [
+                        Bar(
+                            timestamp=index.to_pydatetime(), 
+                            open=float(row['Open'].values[0]), 
+                            high=float(row["High"].values[0]),
+                            low=float(row['Low'].values[0]),
+                            close=float(row['Close'].values[0]),
+                            volume=float(row["Volume"].values[0])
+                            ) for index, row in data.iterrows()
+                ]
+            else:
+                raise NotImplemented("Asset Type not Implemented")
--- src/core/models/bar.py
+++ src/core/models/bar.py
@@ -4,9 +4,9 @@ from typing import Optional
 
 @dataclass(frozen=True)
 class Bar:
-    timestamp: datetime
-    open: float
-    high: float
-    low: float
-    close: float
+    timestamp: datetime = None
+    open: float = None
+    high: float = None
+    low: float = None
+    close: float = None
     volume: Optional[float] = 0.0
