4ae656769846d2e20be35b099e15edc70669961c
JDazogbo <jdazo054@uottawa.ca>
2025-08-16 20:16:35 -0400

    Added monte-carlo permutator

--- src/tests/functional/backtest.py
+++ src/tests/functional/backtest.py
@@ -2,6 +2,7 @@ from src.core.logic.trading_engine import TradingEngine
 from src.core.adapters.yf_market_adapter import YFMarketDataAdapter
 from src.core.logic.moving_average import MovingAverageCrossoverStrategy
 from src.core.adapters.custom_broker_adapter import CustomBrokerAdapter
+from src.core.logic.monte_carlo_permutator import MonteCarloPermutator
 from src.core.models.bar import Bar
 from src.core.models.asset import Asset, AssetType
 from datetime import datetime, timedelta
@@ -10,9 +11,16 @@ if __name__ == "__main__":
     asset = Asset(AssetType.FOREX, "EUR", "USD")
     market_data_adapter = YFMarketDataAdapter(asset)
     market_data_adapter.request_historical_data(asset, datetime.today()-timedelta(365*4), datetime.today())
-    broker_adapter = CustomBrokerAdapter(10000)
-    strategy = MovingAverageCrossoverStrategy(broker_adapter, asset)
-    trading_engine = TradingEngine(broker_adapter, market_data_adapter, [strategy])
-    trading_engine.run(asset, threaded=False)
-    market_data_adapter.generate_data_plot()
+    
+    # Create the Monte Carlo Permutated objects
+    permutator = MonteCarloPermutator(market_data_adapter, 10)
+    broker_results: list[CustomBrokerAdapter] = []
+    for market_adapter_iteration in permutator.permuted_adapters:
+        broker_adapter = CustomBrokerAdapter(10000)
+        strategy = MovingAverageCrossoverStrategy(broker_adapter, asset)
+
+        trading_engine = TradingEngine(broker_adapter, market_adapter_iteration, [strategy])
+        trading_engine.run(asset, threaded=False)
+        market_adapter_iteration.generate_data_plot()
+        broker_results.append(broker_adapter)
     pass
