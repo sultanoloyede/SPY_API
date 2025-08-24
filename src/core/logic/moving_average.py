8befcbfd7995f3aac2c77c6c569c8d1040873cb7
sultanoloyede <sultanoloyede@hotmail.com>
2025-08-24 15:34:40 -0400

    Changed MA strategy allowing fast and slow to be set

--- src/core/logic/moving_average.py
+++ src/core/logic/moving_average.py
@@ -7,61 +7,64 @@ from src.utils.logger import logger
 
 class MovingAverageCrossoverStrategy(Strategy):
 
-    def __init__(self, broker, asset):
+    def __init__(self, broker, asset, fast: int = 50, slow: int = 200):
         super().__init__(broker, asset)
-        self.data_50_sma = []
-        self.data_200_sma = []
 
-    def evaluate(self, bar_data:list[Bar]):
-        """
-        Evaluate the strategy using the latest historical data from the market data port.
-        Executes trades via the broker port if a crossover is detected.
-        """
+        if fast >= slow:
+            raise ValueError("Fast period must be strictly less than slow period.")
 
-        # Compute the current 50 day moving average
-        if len(bar_data) > 200:
-            accumulation_50_day = 0
-            accumulation_200_day = 0
+        self.fast = fast
+        self.slow = slow
+        self.data_fast_sma = []
+        self.data_slow_sma = []
 
-            # Iterate over last 50 bars
-            last_50_bars = bar_data[-50:]
-            sma_50_day = sum(bar.high for bar in last_50_bars) / 50
+    def evaluate(self, bar_data: list[Bar]):
 
-            # Iterate over next 150 bars
-            last_200_bars = bar_data[-200:]
-            sma_200_day = sum(bar.high for bar in last_200_bars) / 200
+        if len(bar_data) > self.slow:
 
-            # Update the 50 day SMA tally
-            self.data_50_sma.append(sma_50_day)
-            while len(self.data_50_sma) > 50:
-                self.data_50_sma.pop(0)
-          
-            # Update the 200 day SMA tally
-            self.data_200_sma.append(sma_200_day)
-            while len(self.data_200_sma) > 200:
-                self.data_200_sma.pop(0)
+            last_fast_bars = bar_data[-self.fast:]
+            sma_fast = sum(bar.high for bar in last_fast_bars) / self.fast
 
-            # Compute current values
+
+            last_slow_bars = bar_data[-self.slow:]
+            sma_slow = sum(bar.high for bar in last_slow_bars) / self.slow
+
+            self.data_fast_sma.append(sma_fast)
+            while len(self.data_fast_sma) > self.fast:
+                self.data_fast_sma.pop(0)
+
+            self.data_slow_sma.append(sma_slow)
+            while len(self.data_slow_sma) > self.slow:
+                self.data_slow_sma.pop(0)
+
             current_bar = bar_data[-1]
-            current_price = current_bar.low            
+            current_price = current_bar.low
             current_date = current_bar.timestamp
 
-            # Compute crossing
             if (
-                len(self.data_50_sma) > 1 and len(self.data_200_sma) > 1 and
-                self.data_50_sma[-1] < self.data_200_sma[-1] and # Death Crossing condition
-                self.data_50_sma[-2] >= self.data_200_sma[-2]
-                ):
-                logger.info(f"Detected Death Cross of 50/200 bar SMA at {current_date}")
+                len(self.data_fast_sma) > 1 and len(self.data_slow_sma) > 1 and
+                self.data_fast_sma[-1] < self.data_slow_sma[-1] and  # Death Cross
+                self.data_fast_sma[-2] >= self.data_slow_sma[-2]
+            ):
+                logger.info(
+                    f"Detected Death Cross of {self.fast}/{self.slow} bar SMA at {current_date}"
+                )
                 self.broker.sell(self.asset, 1, current_price)
+
             elif (
-                len(self.data_50_sma) > 1 and len(self.data_200_sma) > 1 and
-                self.data_50_sma[-1] > self.data_200_sma[-1] and # Golden Crossing condition
-                self.data_50_sma[-2] <= self.data_200_sma[-2]
-                ):
-                logger.info(f"Detected Golden Cross of 50/200 bar SMA at {current_date}")
+                len(self.data_fast_sma) > 1 and len(self.data_slow_sma) > 1 and
+                self.data_fast_sma[-1] > self.data_slow_sma[-1] and  # Golden Cross
+                self.data_fast_sma[-2] <= self.data_slow_sma[-2]
+            ):
+                logger.info(
+                    f"Detected Golden Cross of {self.fast}/{self.slow} bar SMA at {current_date}"
+                )
                 self.broker.buy(self.asset, 1, current_price)
+
             else:
                 pass
         else:
-            logger.debug(f"Insufficient ammount of data to compute 50 and 200 bar moving average. Only {len(bar_data)} bars available.")
+            logger.debug(
+                f"Insufficient ammount of data to compute {self.fast} and {self.slow} "
+                f"bar moving average. Only {len(bar_data)} bars available."
+            )
